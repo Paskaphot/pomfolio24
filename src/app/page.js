ee85e5f5dbe51531dec999a2c97f8b3827fa8545
Paskaphot <ppierre.network@gmail.com>
2024-04-03 17:48:56 +0200

    feat(home-skills): Create static skill card

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import NavHome from "./components/molecules/NavHome";
 import About from "./components/organisms/About";
+import SkillCard from "./components/molecules/SkillCard";
 
 export const metadata = {
 	title: "Pomfolio 2024 index",
@@ -14,7 +15,11 @@ export default function Home() {
 		</header>
 		<main className="home">
 			<About />
-			<section id="skillSection" className="skillSection relative bg-secondary-light"></section>
+			<section id="skillSection" className="skillSection relative bg-secondary-light">
+				<ul className="max-w-7xl px-10 py-40 mx-auto">
+					<li><SkillCard /></li>
+				</ul>
+			</section>
 			<section id="projectSection" className="min-h-screen bg-red-600"></section>
 			<section id="contactSection" className="min-h-screen"></section>
 		</main>
